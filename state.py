a528d4be239fe178f7c27325e0b772d6b038a601
rohit4563 <rohit@rohit.com>
2020-05-25 23:25:44 +0530

    Added new Json Files

---
+++ state.py
@@ -0,0 +1,22 @@
+import json
+
+with open('state.json') as f:
+	data = json.load(f)
+
+
+
+#print(data)
+
+#for state in data['states']:
+#	print(state)
+
+
+for state in data['states']:
+	print(state['name'], state['abbreviation'])
+
+
+for state in data['states']:
+	del state['area_codes']
+
+with open('new_states.json','w') as f:
+	json.dump(data, f, indent=2)
